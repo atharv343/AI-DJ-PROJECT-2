101a83a681065539ac1f94a371141f6091f5d78d
atharv343 <107929877+atharv343@users.noreply.github.com>
2022-09-25 12:40:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 believer="";
 DM="";
 function setup(){
-    canvas=createCanvas(493,343);
-    canvas.position(485,250);
+    canvas=createCanvas(593,443);
     video=createCapture(VIDEO);
     video.hide();
     poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
-    
+    canvas.position(478,200);
 }
+
 function modelLoaded(){
     console.log('Posenet is loaded');}
-function draw(){
-    image(video,485,250,493,343);
+
+    function draw(){
+        image(video,0,0,593,443);
     fill('lime');
     fill('orange');}
     function gotPoses(results){
@@ -20,21 +21,22 @@ function draw(){
             console.log(results);
             scoreRightWrist=results[0].pose.keypoints[10].score*1000;
             scoreLeftWrist=results[0].pose.keypoints[9].score*1000;
-            console.log("scoreRightWrist = "+ scoreRightWrist +"scoreLeftWrist = "+ scoreLeftWrist);}
+            console.log("scoreRightWrist = "+ scoreRightWrist +"scoreLeftWrist = "+ scoreLeftWrist);}}
             
+           
+            scoreLeftWrist=0;
+            scoreRightWrist=0;
+
+
+
+
+
+
+
+
+
+
+
+
+
       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
